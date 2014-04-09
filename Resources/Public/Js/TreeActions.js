8f2e972f471125e896d6491f29d6439bf306245b
Andreas Kiessling <kiessling@pluspol.info>
2014-04-09 08:32:26 +0200

    Ext.apply supports multiple functions to extend an existing class

--- Resources/Public/Js/TreeActions.js
+++ Resources/Public/Js/TreeActions.js
@@ -1,85 +1,70 @@
-Ext.onReady(function() {
-    Ext.apply(TYPO3.Components.PageTree.Actions, {
-        setHiddenInMenus: function(node, tree) {
-            TYPO3.AkPagetreetools.ClickmenuAction.setHiddenInMenus(
-                node.attributes.nodeData,
-                function(response) {
-                    if (this.evaluateResponse(response)) {
-                        this.updateNode(node, node.isExpanded(), response);
-                    }
-                },
-                this
-            );
-        }
-    });
-
-    Ext.apply(TYPO3.Components.PageTree.Actions, {
-        setVisibleInMenus: function(node, tree) {
-            TYPO3.AkPagetreetools.ClickmenuAction.setVisibleInMenus(
-                node.attributes.nodeData,
-                function(response) {
-                    if (this.evaluateResponse(response)) {
-                        this.updateNode(node, node.isExpanded(), response);
-                    }
-                },
-                this
-            );
-        }
-    });
-
-    Ext.apply(TYPO3.Components.PageTree.Actions, {
-        setForceSsl: function(node, tree) {
-            TYPO3.AkPagetreetools.ClickmenuAction.setForceSsl(
-                node.attributes.nodeData,
-                function(response) {
-                    if (this.evaluateResponse(response)) {
-                        this.updateNode(node, node.isExpanded(), response);
-                    }
-                },
-                this
-            );
-        }
-    });
-
-    Ext.apply(TYPO3.Components.PageTree.Actions, {
-        setNoForceSsl: function(node, tree) {
-            TYPO3.AkPagetreetools.ClickmenuAction.setNoForceSsl(
-                node.attributes.nodeData,
-                function(response) {
-                    if (this.evaluateResponse(response)) {
-                        this.updateNode(node, node.isExpanded(), response);
-                    }
-                },
-                this
-            );
-        }
-    });
-
-    Ext.apply(TYPO3.Components.PageTree.Actions, {
-        setExcludeFromSearch: function(node, tree) {
-            TYPO3.AkPagetreetools.ClickmenuAction.setExcludeFromSearch(
-                node.attributes.nodeData,
-                function(response) {
-                    if (this.evaluateResponse(response)) {
-                        this.updateNode(node, node.isExpanded(), response);
-                    }
-                },
-                this
-            );
-        }
-    });
-
-    Ext.apply(TYPO3.Components.PageTree.Actions, {
-        setIncludeInSearch: function(node, tree) {
-            TYPO3.AkPagetreetools.ClickmenuAction.setIncludeInSearch(
-                node.attributes.nodeData,
-                function(response) {
-                    if (this.evaluateResponse(response)) {
-                        this.updateNode(node, node.isExpanded(), response);
-                    }
-                },
-                this
-            );
-        }
-    });
+Ext.onReady(function () {
+  Ext.apply(TYPO3.Components.PageTree.Actions, {
+    setHiddenInMenus: function (node, tree) {
+      TYPO3.AkPagetreetools.ClickmenuAction.setHiddenInMenus(
+          node.attributes.nodeData,
+          function (response) {
+            if (this.evaluateResponse(response)) {
+              this.updateNode(node, node.isExpanded(), response);
+            }
+          },
+          this
+      );
+    },
+    setVisibleInMenus: function (node, tree) {
+      TYPO3.AkPagetreetools.ClickmenuAction.setVisibleInMenus(
+          node.attributes.nodeData,
+          function (response) {
+            if (this.evaluateResponse(response)) {
+              this.updateNode(node, node.isExpanded(), response);
+            }
+          },
+          this
+      );
+    },
+    setForceSsl: function (node, tree) {
+      TYPO3.AkPagetreetools.ClickmenuAction.setForceSsl(
+          node.attributes.nodeData,
+          function (response) {
+            if (this.evaluateResponse(response)) {
+              this.updateNode(node, node.isExpanded(), response);
+            }
+          },
+          this
+      );
+    },
+    setNoForceSsl: function (node, tree) {
+      TYPO3.AkPagetreetools.ClickmenuAction.setNoForceSsl(
+          node.attributes.nodeData,
+          function (response) {
+            if (this.evaluateResponse(response)) {
+              this.updateNode(node, node.isExpanded(), response);
+            }
+          },
+          this
+      );
+    },
+    setExcludeFromSearch: function (node, tree) {
+      TYPO3.AkPagetreetools.ClickmenuAction.setExcludeFromSearch(
+          node.attributes.nodeData,
+          function (response) {
+            if (this.evaluateResponse(response)) {
+              this.updateNode(node, node.isExpanded(), response);
+            }
+          },
+          this
+      );
+    },
+    setIncludeInSearch: function (node, tree) {
+      TYPO3.AkPagetreetools.ClickmenuAction.setIncludeInSearch(
+          node.attributes.nodeData,
+          function (response) {
+            if (this.evaluateResponse(response)) {
+              this.updateNode(node, node.isExpanded(), response);
+            }
+          },
+          this
+      );
+    }
+  });
 });
